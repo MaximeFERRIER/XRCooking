2a4ac9d3841a75a0e639e06b66fee59b82cc88bb
Maxime FERRIER <max.ferrier@free.fr>
2025-01-06 15:36:04 +0100

    Add checkbox for steps progression

--- app/src/main/java/fr/droidfactory/xrcooking/ui/presentation/mealdetails/MealDetailsScreen.kt
+++ app/src/main/java/fr/droidfactory/xrcooking/ui/presentation/mealdetails/MealDetailsScreen.kt
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -16,17 +19,24 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -287,13 +297,30 @@ private fun LazyListScope.stepsScreen(
     }
 
     items(items = steps, key = { item -> "item_$item}" }) {
-        Text(
-            modifier = Modifier.padding(horizontal = 16.dp),
-            text = "- $it",
-            fontSize = 36.sp,
-            color = MaterialTheme.colorScheme.onPrimaryContainer,
-            lineHeight = 36.sp
-        )
+        var isChecked by rememberSaveable { mutableStateOf(false) }
+        Row(
+            modifier = Modifier.fillParentMaxWidth().clickable { isChecked = !isChecked }
+        ) {
+            Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.Center) {
+                Checkbox(
+                    modifier = Modifier.padding(16.dp),
+                    checked = isChecked,
+                    onCheckedChange = {
+                        isChecked = it
+                    }
+                )
+            }
+
+            Text(
+                modifier = Modifier.weight(9f).padding(horizontal = 16.dp),
+                text = it,
+                fontSize = 36.sp,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                lineHeight = 36.sp,
+                textDecoration = if(isChecked) TextDecoration.LineThrough else null
+            )
+        }
+
         Spacer(modifier = Modifier.height(8.dp))
     }
 }
